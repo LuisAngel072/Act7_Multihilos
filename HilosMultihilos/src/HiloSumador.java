5698437146e027a8b603a7b78abd408395982f8b
luisangel072 <luis.delatorre5745@alumnos.udg.mx>
2025-10-08 18:45:35 -0600

    feat(ej3) -> Implementación de lock grueso para contador compartido

--- HilosMultihilos/src/HiloSumador.java
+++ HilosMultihilos/src/HiloSumador.java
@@ -1,8 +1,34 @@
-
 /**
- *
- * @author luis89pun
+ * Hilo sumador
+ * 
+ * Suma un contador compartido según el número de iteraciones especificadas
+ * @author Luis Angel
  */
-public class HiloSumador {
+public class HiloSumador implements Runnable {
+    int num_iteraciones = 0;
+    String noHilo = "";
+    public HiloSumador(int nIteraciones, String nombre) {
+        this.num_iteraciones = nIteraciones;
+        this.noHilo = nombre;
+    }
+    /**
+     * Para manejar la condición de carrera y los locks para la sincronización
+     * al acceso de un recurso compartido, opté por un LOCK GRUESO, bloquea
+     * hasta que el hilo termine de hacer su respectiva tarea en el respectivo
+     * recurso compartido
+     */
+    @Override
+    public void run() {
+        main.lock.lock();
+        try {
+            for(int i = 0; i < this.num_iteraciones; i++) {
+                main.contadorCompartido++;
+            }
+
+        } finally { 
+            main.lock.unlock();
+            System.out.println("El hilo " + noHilo + " finalizó la suma del contador");
+        }
+    }
     
 }
--- HilosMultihilos/src/main.java
+++ HilosMultihilos/src/main.java
@@ -1,13 +1,10 @@
 
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * Ejercicio 2 (Paralelismo):
+ * Ejercicio 3 (Condición de carrera y locks):
+ * Se implementa un LOCK granular para el desarrollo del ejercicio 3
  * @author Luis Angel de la Torre Gómez
  */
 public class main {
@@ -15,57 +12,34 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static BigInteger sumaTotal = BigInteger.ZERO; 
-    public static void main(String[] args) {  
-        
-        // Obtener el número de núcleos o procesadores lógicos disponibles para la JVM
-        int numCores = Runtime.getRuntime().availableProcessors();
-        System.out.println("Cores disponibles: " + numCores);
-        
-        ExecutorService executor = Executors.newFixedThreadPool(4); 
-        
-        int [] num = {1,250,251,500,501,750,751,1000}; //Sacaremos la suma de todos los factoriales de este numero
-        
-        // Lista para guardar los objetos Future, que contendrán los resultados
-        List<Future<BigInteger>> resultados = new ArrayList<>();
-        
-        // Se crearan 4 hilos para cada suma, se dividirá en 4 subpartes igual num
-        for(int i = 0; i < 4; i++) {
-            int inicio = num[i * 2];
-            int fin = num[i * 2 + 1];
-            String noHilo = String.valueOf(i + 1);
-            
-            HiloCalculadorFactorial tarea = new HiloCalculadorFactorial(inicio, fin, noHilo);
-            
-            // Envía la tarea al ExecutorService y guarda el objeto Future
-            Future<BigInteger> futuro = executor.submit(tarea);
-            resultados.add(futuro);
-        }
-        
-        
-        // 3. Agregación: El Hilo Principal espera y suma los resultados
-        System.out.println("\n--- Hilo Principal esperando resultados ---");
-        try {
-            for (Future<BigInteger> futuro : resultados) {
-                //.get() Bloquea el Hilo Principal hasta que los HilosCalculadorFactorial finalicen
-                // El Hilo Principal espera a que todos los hilos terminen.
-                BigInteger sumaParcial = futuro.get(); 
-                
-                //Suma los resultados parciales al total
-                sumaTotal = sumaTotal.add(sumaParcial); 
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // Es crucial apagar el ExecutorService
-            executor.shutdown();
-        }
-        
-        // 4. Mostrar el Resultado Final
-        System.out.println("\n==============================================");
-        System.out.println("Suma final de los factoriales: (1! + ... + 1000!)");
-        System.out.println(sumaTotal);
-        System.out.println("==============================================");
-    }
+    static int contadorCompartido = 0; //Contador compartido
+    static Lock lock = new ReentrantLock(false);
     
+    public static void main(String[] args) {  
+        int numHilos = 10;
+        Thread[] hilosSuma = new Thread[numHilos];
+        
+        int num_iteraciones = 100_000;
+        
+        for (int i = 0; i < numHilos; i++) {
+            String noHilo = String.valueOf(i+1);
+            hilosSuma[i] = new Thread(new HiloSumador(num_iteraciones, noHilo));
+        }
+        
+        for (int i = 0; i < numHilos; i++) {
+            hilosSuma[i].start();
+        }
+        
+        // Esperar a que todos terminen
+        for (int i = 0; i < numHilos; i++) {
+            try {
+                hilosSuma[i].join(); //Espera que el hilo termine
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        //El resultado esperado debe ser de 1_000_000 (un millón)
+        System.out.println("El resultado final del contador compartido es: " + contadorCompartido);
+    }
 }
